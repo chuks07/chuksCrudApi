71f601dc590b1af838b23671063a86a21e9b0ca0
chuksumenwa@gmail.com <chuksumenwa@gmail.com>
2021-09-08 00:57:24 +0100

    crudApi

--- main.go
+++ main.go
@@ -1,6 +1,31 @@
 package main
-import "fmt"
+
+import( 
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+	"log"
+	"time"
+	"context"
+)
+type Book struct{
+	Title string `json:"Title"`
+	Author string `json:"Author"`
+	Year int `json:"Year"`
+	Category string `json:"Category"`
+}
 
 func main(){
 	fmt.Println("Hello world")
+}
+
+func createBookHandler (c *gin.Context){
+	var book Book
+
+	err := c.ShouldBindJSON(&book)
+	if err !=nil{
+		c.JSON(400,gin.H{
+			"error": "invalid data request",
+		})
+	}
 }
